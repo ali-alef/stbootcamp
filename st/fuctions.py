bf666213c2eb23501c28be4e1139ee7e9cd35ab7
alialef <alialefkhani77@gmail.com>
2022-08-18 12:54:57 +0430

    wtf

---
+++ st/fuctions.py
@@ -0,0 +1,39 @@
+from .models import *
+
+
+def applyAction(rule, price):
+    f = rule.action.fixedDisplacementAmount
+    p = rule.action.percentageDisplacementAmount
+    m = rule.action.maximumDisplacementAmount
+
+    if rule.type == "MARKUP":
+        displacement = min(f + (price * p) / 100, m)
+        newPrice = price + displacement
+    else:
+        displacement = min(f + (price * p) / 100, m)
+        newPrice = price - displacement
+
+    return displacement, newPrice
+
+
+def checkRules(userType, price, appliedRules):
+    sequence = 1
+
+    for rule in Rule.objects.all():
+        if ruleCondition(rule, userType):
+            displacement, newPrice = applyAction(rule, price)
+            dict = {
+                "rule": rule,
+                "oldPrice": price,
+                "newPrice": newPrice,
+                "displacement": displacement,
+                "sequence": sequence
+            }
+
+            sequence += 1
+
+            appliedRules.append(dict)
+
+
+def ruleCondition(rule, userType):
+    pass
--- st/views.py
+++ st/views.py
@@ -1,16 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from .models import *
+from .fuctions import checkRules
 
 
 def apply(request):
     userType = request.POST['userType']
     price = request.POST['price']
+    appliedRules = []
+
+    checkRules(userType, price, appliedRules)
 
 
-def applyAction(rule, price):
-    f = rule.action.fixedDisplacementAmount
-    p = rule.action.percentageDisplacementAmount
-    m = rule.action.maximumDisplacementAmount
-
-    if rule.type
