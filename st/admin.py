6de266491c0b25252382364b8f3f6618cd04bf96
Sara <58365375+dimsra@users.noreply.github.com>
2022-08-18 17:09:32 +0430

    Update admin.py

--- st/admin.py
+++ st/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Rule)
-admin.site.register(Action)
-admin.site.register(Condition)
+class RuleAdmin(admin.ModelAdmin):
+    list_display = ('id ', 'name', 'type')
+
+class ConditionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'rule', 'type', 'value')
+
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fixedDisplacementAmount', 'percentageDisplacementAmount', 'maximumDisplacementAmount')
+
+
+admin.site.register(Rule, RuleAdmin)
+admin.site.register(Action, ActionAdmin)
+admin.site.register(Condition, ConditionAdmin)
